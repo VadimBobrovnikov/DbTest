86e0b495a9be3a15fbcf0c47e21dd8d6c3c96996
4215 Бобровников В. В. <b.vadim29@gmail.com>
2023-12-10 22:48:37 +0300

    First

---
+++ app/src/main/java/com/example/myapplication/Item.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "items")
+data class Item (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    @ColumnInfo(name = "name")
+    var name: String,
+    @ColumnInfo(name = "tel_num")
+    var tel_num: String,
+
+    )
---
+++ app/src/main/java/com/example/myapplication/MainDB.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1)
+abstract class MainDB: RoomDatabase() {
+
+    abstract fun getDao(): Dao
+
+    companion object{
+        fun getDb(context: Context): MainDB{
+            return Room.databaseBuilder(
+                context.applicationContext,
+                MainDB::class.java,
+                "test.db"
+            ).build()
+        }
+    }
+}
